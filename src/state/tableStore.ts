0e7cc85fc6f90c6992b9dfd1482f9d43fac7e946
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-01 22:16:01 +0100

    chore: remove boiler plate method generation

--- src/state/tableStore.ts
+++ src/state/tableStore.ts
@@ -3,31 +3,6 @@ import { SlabType } from '../types/slabType';
 import { UserCategory } from '../enums/user';
 import { StartData } from './data';
 
-const createSlab = (): SlabType => ({
-  id: Math.round(Math.random() * 100000).toString(16),
-  planReference: 'Plane',
-  location_x: Math.random() * 100,
-  location_y: Math.random() * 100,
-  location_z: Math.random() * 100,
-  strength: 'C25/30',
-  dimensions_l: Math.random() * 100,
-  dimensions_w: Math.random() * 100,
-  dimensions_h: Math.random() * 100,
-  liveload: Math.random() * 100,
-  typeOfElement: 'type of element',
-  rebarDiameterTop: Math.random() * 100,
-  rebarAmountTop: Math.random() * 100,
-  rebarDiameterBottom: Math.random() * 100,
-  rebarAmountBottom: Math.random() * 100,
-});
-
-const createSlabs = () => {
-  const slabs = [];
-  for (let i = 0; i < 10; i++) slabs.push(createSlab());
-
-  return slabs;
-};
-
 type TableStore = {
   elements: SlabType[];
   updateElement: (element: SlabType) => void;
