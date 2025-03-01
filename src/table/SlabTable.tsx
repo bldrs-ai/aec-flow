bb07ac13bf1cc5eecbebdb309678f2749676d0bd
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-01 22:10:30 +0100

    new logic for constructing data

--- src/table/SlabTable.tsx
+++ src/table/SlabTable.tsx
@@ -1,10 +1,10 @@
 import { Table } from 'antd';
 import { useTableStore } from '../state/tableStore';
-import { columns } from './columnTypes';
+import { columnTypeMap, defaultValues } from './columnTypes';
 
 export const SlabTable: React.FC = () => {
   const elements = useTableStore((s) => s.elements);
   const userCategory = useTableStore((s) => s.userCategory);
 
-  return <Table dataSource={elements} columns={columns[userCategory]}></Table>;
+  return <Table dataSource={elements} columns={defaultValues[userCategory].map((s) => columnTypeMap[s])}></Table>;
 };
