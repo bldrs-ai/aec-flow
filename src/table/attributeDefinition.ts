da20cc90d9e7c51d1f1618b34ebc5e7ad72c52d9
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-02 00:39:57 +0100

    chore: extracted colum view definitions

---
+++ src/table/attributeDefinition.ts
@@ -0,0 +1,56 @@
+import { UserCategory } from '../enums/user';
+import { SlabType } from '../types/slabType';
+
+const CostumUIKeys = ['location', 'rebarRenderer', 'type', 'weight', 'count'];
+export const PartTypeKeys = [
+  'id',
+  'planReference',
+  'location_x',
+  'location_y',
+  'location_z',
+  'strength',
+  'dimensions_l',
+  'dimensions_w',
+  'dimensions_h',
+  'liveload',
+  'typeOfElement',
+  'rebarDiameterTop',
+  'rebarAmountTop',
+  'rebarDiameterBottom',
+  'rebarAmountBottom',
+];
+
+export const AllDefinedRenders = [...CostumUIKeys, ...PartTypeKeys];
+export const RenderLocal: Record<string, string> = {
+  id: 'Id',
+  planReference: 'Plan Reference',
+  type: 'Type',
+  location_x: 'X Coordinate',
+  location_y: 'Y Coordinate',
+  location_z: 'Z Coordinate',
+  strength: 'Strength',
+  dimensions_l: 'Length',
+  dimensions_w: 'Width',
+  dimensions_h: 'Height',
+  weight: 'Weight',
+  liveload: 'Live Load',
+  typeOfElement: 'Element Type',
+  rebarDiameterTop: 'Rebar Diameter Top',
+  rebarAmountTop: 'Rebar Amount Top',
+  rebarDiameterBottom: 'Rebar Diameter Bottom',
+  rebarAmountBottom: 'Rebar Diameter Bottom',
+  location: 'Location',
+  rebarRenderer: 'Rebar',
+  count: 'Count',
+};
+
+const architect = [...CostumUIKeys, 'count'];
+
+export const DefaultRenderValues: Record<UserCategory, string[]> = {
+  [UserCategory.Ubermensch]: AllDefinedRenders,
+  [UserCategory.Architect]: architect,
+  [UserCategory.Engineer]: [],
+  [UserCategory.Client]: ['type', 'typeOfElement', 'count'],
+  [UserCategory.Contracter]: [],
+};
+
--- src/table/columnTypes.tsx
+++ src/table/columnTypes.tsx
@@ -1,49 +1,5 @@
 import { ColumnType } from 'antd/es/table';
-import { UserCategory } from '../enums/user';
 import { SlabType } from '../types/slabType';
-
-const CostumUIKeys = ['location', 'rebarRenderer', 'type', 'weight'];
-const PartTypeKeys = [
-  'id',
-  'planReference',
-  'location_x',
-  'location_y',
-  'location_z',
-  'strength',
-  'dimensions_l',
-  'dimensions_w',
-  'dimensions_h',
-  'liveload',
-  'typeOfElement',
-  'rebarDiameterTop',
-  'rebarAmountTop',
-  'rebarDiameterBottom',
-  'rebarAmountBottom',
-];
-
-export const AllAvailableColumns = [...CostumUIKeys, ...PartTypeKeys];
-
-const local: Record<string, string> = {
-  id: 'Id',
-  planReference: 'Plan Reference',
-  type: 'Type',
-  location_x: 'X Coordinate',
-  location_y: 'Y Coordinate',
-  location_z: 'Z Coordinate',
-  strength: 'Strength',
-  dimensions_l: 'Length',
-  dimensions_w: 'Width',
-  dimensions_h: 'Height',
-  weight: 'Weight',
-  liveload: 'Live Load',
-  typeOfElement: 'Element Type',
-  rebarDiameterTop: 'Rebar Diameter Top',
-  rebarAmountTop: 'Rebar Amount Top',
-  rebarDiameterBottom: 'Rebar Diameter Bottom',
-  rebarAmountBottom: 'Rebar Diameter Bottom',
-  location: 'Location',
-  rebarRenderer: 'Rebar',
-};
 
 export const columnTypeMap: { [attribute: string]: ColumnType<SlabType> } = {
   ...Object.fromEntries(
@@ -89,13 +45,4 @@ export const columnTypeMap: { [attribute: string]: ColumnType<SlabType> } = {
     render: (_, element) => `${(element.dimensions_l * element.dimensions_w * element.dimensions_h * 0.6 * 0.0000025).toFixed(0)} kg`,
   },
 };
-
-const architect = CostumUIKeys;
-
-export const defaultValues: Record<UserCategory, string[]> = {
-  [UserCategory.Ubermensch]: [...PartTypeKeys, ...CostumUIKeys],
-  [UserCategory.Architect]: architect,
-  [UserCategory.Engineer]: [],
-  [UserCategory.Client]: [],
-  [UserCategory.Contracter]: [],
 };
