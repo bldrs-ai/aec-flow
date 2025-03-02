9abc4333d62487739b5b69ef7651091675bb0af4
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-02 14:23:41 +0100

    feat: viewer canvas added

--- src/userView/SettingsAndFilterPanel.tsx
+++ src/userView/SettingsAndFilterPanel.tsx
@@ -6,13 +6,10 @@ export const SettingsAndFilterPanel: React.FC = () => (
     style={{
       display: 'flex',
       top: 0,
-      right: 0,
       left: 0,
       padding: 10,
       position: 'fixed',
       zIndex: 10,
-      width: '100%',
-      backgroundColor: 'white',
       flexDirection: 'row',
       gap: 12,
     }}
