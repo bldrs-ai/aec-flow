7fee6ea7ffcddb081fa593e076796ff36428410e
JonasWard <43600893+JonasWard@users.noreply.github.com>
2025-03-02 14:20:07 +0100

    feat: added view

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from 'react';
 import { SlabTable } from './table/SlabTable';
+import { initScene } from './viewer/viewer';
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    initScene();
+  }, []);
+
   return <SlabTable />;
 };
